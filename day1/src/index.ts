cece319ee696a977d9972c6532b7adfb8f26b0c7
FinnyMarigold58 <59434139+FinnyMarigold58@users.noreply.github.com>
2023-12-02 20:12:47 -0600

    Bad Part 2 (works)

--- day1/src/index.ts
+++ day1/src/index.ts
@@ -1,15 +1,42 @@
 import * as fs from "fs";
 import path from "path";
 
-const fullFile = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
-const lines = fullFile.split("\r\n");
+let fullFile = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+
+const lines = fullFile.split("\n");
+
 const digits = parseInput(lines);
 
 const digitSum = digits.reduce((acc: number, digit: number): number => {
   return acc + digit;
 }, 0);
 
+console.log("Part 1");
 console.log("Sum: " + digitSum);
+
+// Part 2
+
+// Idea of replacing with one1one, two2two, etc from
+fullFile = fullFile.replace(/one/g, "one1one");
+fullFile = fullFile.replace(/two/g, "two2two");
+fullFile = fullFile.replace(/three/g, "three3three");
+fullFile = fullFile.replace(/four/g, "four4four");
+fullFile = fullFile.replace(/five/g, "five5five");
+fullFile = fullFile.replace(/six/g, "six6six");
+fullFile = fullFile.replace(/seven/g, "seven7seven");
+fullFile = fullFile.replace(/eight/g, "eight8eight");
+fullFile = fullFile.replace(/nine/g, "nine9nine");
+
+let lines2 = fullFile.split("\n");
+
+const digits2 = parseInput(lines2);
+
+const digitSum2 = digits2.reduce((acc: number, digit: number): number => {
+  return acc + digit;
+}, 0);
+
+console.log("Part 2");
+console.log("Sum: " + digitSum2);
 
 /**
  * Search through string for numbers
@@ -23,6 +50,8 @@ function parseInput(input: string[]): number[] {
     const extracted = value.match(expression) ?? [];
     foundNumbers.push(Number(extracted[0] + extracted[extracted.length - 1]));
   });
+
+  console.log(foundNumbers);
 
   return foundNumbers;
 }
