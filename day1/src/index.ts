1515f0fe06c5ce715199a372e21489ddf1e31c8d
FinnyMarigold58 <59434139+FinnyMarigold58@users.noreply.github.com>
2023-12-02 19:40:08 -0600

    Forgot to stage these ;-;

--- day1/src/index.ts
+++ day1/src/index.ts
@@ -1,9 +1,28 @@
 import * as fs from "fs";
+import path from "path";
 
-const fullFile: string = fs.readFileSync("input.txt", "utf-8");
-const separatedCodes = fullFile.split("\r\n");
-console.log(separatedCodes);
+const fullFile = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+const lines = fullFile.split("\r\n");
+const digits = parseInput(lines);
 
-function parseInput(input: String[]): Number[] {
-  return [];
+const digitSum = digits.reduce((acc: number, digit: number): number => {
+  return acc + digit;
+}, 0);
+
+console.log("Sum: " + digitSum);
+
+/**
+ * Search through string for numbers
+ * @param input Lines to search for Numbers
+ * @returns Array of Numbers in input
+ */
+function parseInput(input: string[]): number[] {
+  const foundNumbers: number[] = [];
+  const expression = /\d/g;
+  input.forEach((value) => {
+    const extracted = value.match(expression) ?? [];
+    foundNumbers.push(Number(extracted[0] + extracted[extracted.length - 1]));
+  });
+
+  return foundNumbers;
 }
