579e69a4585b5da65057f3f93e39fff99941218f
FinnyMarigold58 <59434139+FinnyMarigold58@users.noreply.github.com>
2023-12-02 20:21:45 -0600

    remove unneeded letters

--- day1/src/index.ts
+++ day1/src/index.ts
@@ -16,16 +16,16 @@ console.log("Sum: " + digitSum);
 
 // Part 2
 
-// Idea of replacing with one1one, two2two, etc from
-fullFile = fullFile.replace(/one/g, "one1one");
-fullFile = fullFile.replace(/two/g, "two2two");
-fullFile = fullFile.replace(/three/g, "three3three");
-fullFile = fullFile.replace(/four/g, "four4four");
-fullFile = fullFile.replace(/five/g, "five5five");
-fullFile = fullFile.replace(/six/g, "six6six");
-fullFile = fullFile.replace(/seven/g, "seven7seven");
-fullFile = fullFile.replace(/eight/g, "eight8eight");
-fullFile = fullFile.replace(/nine/g, "nine9nine");
+// Idea of replacing with one1one, two2two, etc from Piplup7575
+fullFile = fullFile.replace(/one/g, "o1e");
+fullFile = fullFile.replace(/two/g, "t2o");
+fullFile = fullFile.replace(/three/g, "t3e");
+fullFile = fullFile.replace(/four/g, "f4r");
+fullFile = fullFile.replace(/five/g, "f5e");
+fullFile = fullFile.replace(/six/g, "s6x");
+fullFile = fullFile.replace(/seven/g, "s7n");
+fullFile = fullFile.replace(/eight/g, "e8t");
+fullFile = fullFile.replace(/nine/g, "n9e");
 
 let lines2 = fullFile.split("\n");
 
@@ -37,7 +37,6 @@ const digitSum2 = digits2.reduce((acc: number, digit: number): number => {
 
 console.log("Part 2");
 console.log("Sum: " + digitSum2);
-
 /**
  * Search through string for numbers
  * @param input Lines to search for Numbers
@@ -50,8 +49,6 @@ function parseInput(input: string[]): number[] {
     const extracted = value.match(expression) ?? [];
     foundNumbers.push(Number(extracted[0] + extracted[extracted.length - 1]));
   });
-
-  console.log(foundNumbers);
 
   return foundNumbers;
 }
