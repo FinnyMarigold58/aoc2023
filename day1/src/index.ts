3877a27cc325d8d5e410e98c43da15522cdd54c6
FinnyMarigold58 <59434139+FinnyMarigold58@users.noreply.github.com>
2023-12-02 21:48:32 -0600

    Restructure Project

--- day1/src/index.ts
+++ day1/src/index.ts
@@ -1,54 +1,58 @@
-import * as fs from "fs";
-import path from "path";
+import { readFileSync } from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-let fullFile = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const lines = fullFile.split("\n");
+let fullFile = readFileSync(path.join(__dirname, "input.txt"), "utf-8")
 
-const digits = parseInput(lines);
+const lines = fullFile.split("\n")
+
+const digits = parseInput(lines)
 
 const digitSum = digits.reduce((acc: number, digit: number): number => {
-  return acc + digit;
-}, 0);
+	return acc + digit
+}, 0)
 
-console.log("Part 1");
-console.log("Sum: " + digitSum);
+console.log("Part 1")
+console.log(`Sum: ${digitSum}`)
 
 // Part 2
 
 // Idea of replacing with one1one, two2two, etc from Piplup7575
-fullFile = fullFile.replace(/one/g, "o1e");
-fullFile = fullFile.replace(/two/g, "t2o");
-fullFile = fullFile.replace(/three/g, "t3e");
-fullFile = fullFile.replace(/four/g, "f4r");
-fullFile = fullFile.replace(/five/g, "f5e");
-fullFile = fullFile.replace(/six/g, "s6x");
-fullFile = fullFile.replace(/seven/g, "s7n");
-fullFile = fullFile.replace(/eight/g, "e8t");
-fullFile = fullFile.replace(/nine/g, "n9e");
+fullFile = fullFile.replace(/one/g, "o1e")
+fullFile = fullFile.replace(/two/g, "t2o")
+fullFile = fullFile.replace(/three/g, "t3e")
+fullFile = fullFile.replace(/four/g, "f4r")
+fullFile = fullFile.replace(/five/g, "f5e")
+fullFile = fullFile.replace(/six/g, "s6x")
+fullFile = fullFile.replace(/seven/g, "s7n")
+fullFile = fullFile.replace(/eight/g, "e8t")
+fullFile = fullFile.replace(/nine/g, "n9e")
 
-let lines2 = fullFile.split("\n");
+const lines2 = fullFile.split("\n")
 
-const digits2 = parseInput(lines2);
+const digits2 = parseInput(lines2)
 
 const digitSum2 = digits2.reduce((acc: number, digit: number): number => {
-  return acc + digit;
-}, 0);
+	return acc + digit
+}, 0)
 
-console.log("Part 2");
-console.log("Sum: " + digitSum2);
+console.log("Part 2")
+console.log(`Sum: ${digitSum2}`)
 /**
  * Search through string for numbers
  * @param input Lines to search for Numbers
  * @returns Array of Numbers in input
  */
 function parseInput(input: string[]): number[] {
-  const foundNumbers: number[] = [];
-  const expression = /\d/g;
-  input.forEach((value) => {
-    const extracted = value.match(expression) ?? [];
-    foundNumbers.push(Number(extracted[0] + extracted[extracted.length - 1]));
-  });
+	const foundNumbers: number[] = []
+	const expression = /\d/g
 
-  return foundNumbers;
+	for (const value of input) {
+		const extracted = value.match(expression) ?? []
+		foundNumbers.push(Number(extracted[0] + extracted[extracted.length - 1]))
+	}
+
+	return foundNumbers
 }
